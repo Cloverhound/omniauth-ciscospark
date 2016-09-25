1091a28bc8d1397802abb87ebcd69422e3048b09
CHAD STACHOWICZ <chadstachowicz@CHADs-iMac.local>
2016-09-24 22:36:52 -0400

    fixing URL

--- lib/omniauth-ciscospark/version.rb
+++ lib/omniauth-ciscospark/version.rb
@@ -1,5 +1,5 @@
 module OmniAuth
   module Ciscospark
-    VERSION = "0.1.6"
+    VERSION = "0.1.7"
   end
 end
--- lib/omniauth/strategies/ciscospark.rb
+++ lib/omniauth/strategies/ciscospark.rb
@@ -8,7 +8,11 @@ module OmniAuth
 
       # This is where you pass the options you would pass when
       # initializing your consumer from the OAuth gem.
-      option :client_options, {:site => "https://api.ciscospark.com/v1/authorize"}
+      option :client_options, {
+  :site          => 'https://api.ciscospark.com',
+  :authorize_url => '/v1/authorize',
+  :token_url     => '/v1/access_token'
+}
       option :scope, "spark:messages_write spark:rooms_read spark:teams_read spark:memberships_read spark:messages_read spark:rooms_write spark:people_read spark:kms spark:memberships_write spark:teams_write park:team_memberships_read spark:team_memberships_write"
 
       # These are called after authentication has succeeded. If
